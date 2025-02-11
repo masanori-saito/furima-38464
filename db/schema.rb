6e644372ba5afce7cf19ba2ac8eb5119e66ea3bb
masanori-saito <108559782+masanori-saito@users.noreply.github.com>
2025-02-11 22:03:45 +0900

    修正内容編集

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2025_01_11_085354) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
