ea4f874e2db19de83de4d2f37ac32b4b7c723d05
masanori-saito <108559782+masanori-saito@users.noreply.github.com>
2025-03-11 22:02:48 +0900

    カテゴリーに関するバリデーションを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :item_status
+  with_options presence: true do
+    validates :category_id
+
+  end
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+  end
 end
