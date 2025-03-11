3fd00f9238907576f22b767e83041ae119684d48
masanori-saito <108559782+masanori-saito@users.noreply.github.com>
2025-03-12 01:08:23 +0900

    出品した商品の情報をテーブルに保存できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,5 +19,6 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
+
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,28 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
   with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
     validates :category_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    
 
   end
   with_options numericality: { other_than: 0 } do
     validates :category_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
   end
 end
