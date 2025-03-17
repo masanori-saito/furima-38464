b3656903bdec70604261c59259e06fd4e7986722
masanori-saito <108559782+masanori-saito@users.noreply.github.com>
2025-03-17 15:57:15 +0900

    テストコード実装

--- Gemfile
+++ Gemfile
@@ -31,7 +31,6 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'faker'
   gem 'capistrano'
@@ -39,6 +38,7 @@ group :development, :test do
   gem 'capistrano-bundler'
   gem 'capistrano-rails'
   gem 'capistrano3-unicorn'
+  gem 'rspec-rails', '~> 4.0.0'
 end
 
 group :development do
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
+    # テーブルとのアソシエーション
+    belongs_to :user
+
   has_one_attached :image
   belongs_to :category
   belongs_to :item_status
@@ -17,15 +20,15 @@ class Item < ApplicationRecord
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
+    validates :item_status_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    
-
-  end
+   end
   with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
+    validates :item_status_id
   end
 end
