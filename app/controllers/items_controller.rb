fc2ba869fde6ea4ecbb56f0433547c7516747ac8
masanori-saito <108559782+masanori-saito@users.noreply.github.com>
2025-04-01 22:14:57 +0900

    商品情報編集機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
     # ログインしていないユーザーはログインページに促す
     before_action :authenticate_user!, except: [:index, :show]
+
+      # 重複処理をまとめる
+  before_action :set_item, only: [:show, :edit, :update]
     
   def new
     @item = Item.new
@@ -20,7 +23,27 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
+  end
+
+  def edit
+
+    # ログインしているユーザーと同一であればeditファイルが読み込まれる
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    # バリデーションがOKであれば詳細画面へ
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
+      render 'edit'
+    end
   end
 
   private
@@ -28,5 +51,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
                                  :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
