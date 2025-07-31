2ea9256309b64b822f31f2fbd327b57f21423d12
masanori-saito <108559782+masanori-saito@users.noreply.github.com>
2025-07-31 21:26:34 +0900

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
-// sass-loaderの設定を取得
-const sassLoader = environment.loaders.get('sass');
-const sassLoaderConfig = sassLoader.use.find(loader => loader.loader === 'sass-loader');
+// // sass-loaderの設定を取得
+// const sassLoader = environment.loaders.get('sass');
+// const sassLoaderConfig = sassLoader.use.find(loader => loader.loader === 'sass-loader');
 
-// dart-sass (sassパッケージ) を使うように設定
-sassLoaderConfig.options = {
-  implementation: require('sass'),
-  webpackImporter: false,
-};
+// // dart-sass (sassパッケージ) を使うように設定
+// sassLoaderConfig.options = {
+//   implementation: require('sass'),
+//   webpackImporter: false,
+// };
 
+const sassLoader = environment.loaders.get('sass')
+sassLoader.use.find(item => item.loader === 'sass-loader').options.implementation = require('sass')
 
 module.exports = environment
