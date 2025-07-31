3d362aa0938c07cf93d070ff00e4ab763fbbae90
masanori-saito <108559782+masanori-saito@users.noreply.github.com>
2025-07-31 21:20:46 +0900

    sass

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+// sass-loaderの設定を取得
+const sassLoader = environment.loaders.get('sass');
+const sassLoaderConfig = sassLoader.use.find(loader => loader.loader === 'sass-loader');
+
+// dart-sass (sassパッケージ) を使うように設定
+sassLoaderConfig.options = {
+  implementation: require('sass'),
+  webpackImporter: false,
+};
+
+
 module.exports = environment
